c51ee9afc1dccaa0251169e46043841d24c282e8
finnbjornth <finnbjorn@finnbjorn.com>
2013-06-30 15:36:35 +0000

    Fixed as asked for in code review comment from 'thordurorn' to commit '90e70b7d5150d847f6ae56d1c468d93c5b4d460b'. JSON response fixed and URL fixed so it complies with REST.

--- rate-extensions.py
+++ rate-extensions.py
@@ -3,24 +3,32 @@ import webapp2
 import json
 
 class MainPage(webapp2.RequestHandler):
+    """As this application is not about a webpage
+       only a simple message is shown on the main
+       page."""
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Not a site!')
 
-class REapi(webapp2.RequestHandler):
+class GetApi(webapp2.RequestHandler):
     """REapi takes JSON string from the Rate-Extension
        Chrome extension, checks the permissions and
-       returns status."""
+       returns the status as JSON."""
     def get(self):
         response = {}
-        stuff = [{"enabled" : "true"}, {"category" : "Snoop"}, {"rank" : "Small Fry"}]
-        response["Facebook Notifications"] = stuff
-        print response
+        # Only a stub at the moment.
+        extensions = [
+             {"name":"Facebook notification","enabled":"true","category":"Snoop","rank":"Small Fry"}
+             ,{"name":"Herpa Derpa","enabled":"true","category":"Snoop","rank":"Small Fry"}
+             ,{"name":"Merpa skerf","enabled":"true","category":"Snoop","rank":"Small Fry"}
+             ,{"name":"Skerfa smurfa","enabled":"true","category":"Snoop","rank":"Small Fry"}
+        ]
+        response["extensions"] = extensions
 
         self.response.headers['Content-Type'] = "application/json"
         self.response.out.write(json.dumps(response))
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/api/v1/', REapi),
+    ('/api/v1/get', GetApi),
 ], debug=True)
