90e70b7d5150d847f6ae56d1c468d93c5b4d460b
unknown <finnbjorn@Developement.family.home>
2013-06-30 15:00:13 +0000

    Stubbar út deafult svar, sama og var í hönnunarskjalinu. Nóg til að byrja ða vinna með

---
+++ rate-extensions.py
@@ -0,0 +1,31 @@
+#from google.appengine.api import users
+import webapp2
+import json
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        #user = users.get_current_user()
+
+        #if user:
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Not a site!')
+        #else:
+        #    self.redirect(users.create_login_url(self.request.uri))
+
+class REapi(webapp2.RequestHandler):
+    """REapi takes JSON string from the Rate-Extension
+       Chrome extension, checks the permissions and
+       returns status."""
+    def get(self):
+        response = {}
+        stuff = [{"enabled" : "true"}, {"category" : "Snoop"}, {"rank" : "Small Fry"}]
+        response["Facebook Notifications"] = stuff
+        print response
+
+        self.response.headers['Content-Type'] = "application/json"
+        self.response.out.write(json.dumps(response))
+
+application = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ('/api/v1/', REapi),
+], debug=True)
